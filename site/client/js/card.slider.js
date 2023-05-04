7c3894f6d441e21dcb57d3d7d6c2736405f1ecff
StacyMch <greensummertea@gmail.com>
2023-05-04 23:56:16 +0300

    card responsive

--- site/client/js/card.slider.js
+++ site/client/js/card.slider.js
@@ -7,7 +7,14 @@
     slidesToScroll: 1,
     arrows: false,
     fade: true,
-    asNavFor: '.slider-nav'
+    asNavFor: '.slider-nav',
+    // Адаптивность
+    // responsive: [
+       
+    // {
+    //   breakpoint: 300,
+    //   settings: "unslick" // destroys slick
+    // }]
   });
   $('.slider-nav').slick({
     slidesToShow: 3,
@@ -16,7 +23,31 @@
     dots: true,
     centerMode: true,
     focusOnSelect: true,
+    autoplay: true,
     prevArrow: '<div class="btn-slider-prev">&#10094</div>',  //стрелка назад
     nextArrow: '<div class="btn-slider-next">&#10095</div>',  //стрелка вперёд
+
+    // Адаптивность
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 3,
+          infinite: true
+        }
+    }, 
+    {
+      breakpoint: 650,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+    }, 
+    // {
+    //   breakpoint: 450,
+    //   settings: "unslick" // destroys slick
+    // }, 
+    ]
+
   });
     
