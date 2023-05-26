accba80db8d96b58c3f07efcc2adf4ed8b5d3ef8
chemezoida <evchemez@mail.ru>
2023-05-26 11:04:41 +0300

    адаптив туров на главной

--- site/index.php
+++ site/index.php
@@ -282,7 +282,7 @@
 
                     foreach ($toures as $tour) { ?>
 
-                        <div class="d-flex card-item slide catalog-card tour-item height-card-item tour-card">
+                        <div class="d-flex card-item slide catalog-card tour-item height-card-item tour-card tour-card-margin">
                             <div class="tour-item__img-container">
                                 <div class="tour-item__img" style="background-image: url('client/<?= $tour['img-title'];?>');"> </div>
                             </div>
