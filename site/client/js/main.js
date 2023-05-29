ee95abb6e5269740109b629b0ed2a346f6efdf95
StacyMch <greensummertea@gmail.com>
2023-05-29 19:48:56 +0300

    функционал формы обратной связи

--- site/client/js/main.js
+++ site/client/js/main.js
@@ -27,12 +27,10 @@ for (let i = 0; i < triggers.length; i++) {
   });
 }
 
-
 /* ******** СКРЫТЬ/ПОКАЗАТЬ форму обратной связи ******** */
 const modalFeedbackForm = document.querySelector(".modalbox");
 
 function showFeedback() {
-    console.log("jjhhjgjhg");
     modalFeedbackForm.classList.remove('d-none');
 }
 
@@ -48,15 +46,84 @@ function closeFeedbackIcon() {
 
 modalFeedbackForm.addEventListener("click", closeFeedback);
 
+/* ******** ПОКАЗАТЬ поп-ап об успешно отправленной заявке ******** */
+const modalFeedbackFormSuccess = document.getElementById("success");
 
-/* ******** Отправка в телеграмм-бота ******** */
-function sendExample() {
+function closeFeedbackIconSuccess() {
+    modalFeedbackFormSuccess.classList.add('d-none');
+}
 
-    //предотвратить дефолтные действия, отмена отправки формы
+function closeFeedback() {
+    if (this === event.target) {
+        modalFeedbackFormSuccess.classList.add('d-none');
+    }
+}
+
+modalFeedbackFormSuccess.addEventListener("click", closeFeedback);
+
+
+/* ******** Отправка заявки в телеграмм-бота ******** */
+function sendForm() {
+
+    //предотвратить дефолтные действия, отмена отправки формы (чтобы страница не перезагружалась)
     event.preventDefault(); 
 
-    alert("сообщение отправлено");
+    let name = document.getElementById('name').value;
+    let phone = document.getElementById('phone').value;
+    let mail = document.getElementById('mail').value;
 
-    //скрыть форму
-    modalFeedbackForm.classList.add('d-none');
+    //проверки, чтобы нужные поля были заполнены
+    if (name.trim() != '') {
+
+        if (phone.trim() != '' || mail.trim() != '') {
+
+            //создание объекта, отправляющего запросы
+            let requestObj = new XMLHttpRequest();
+        
+            let link;
+        
+                if (phone && !mail) {
+                    if (phone.trim() != '') {
+                        link = 'https://api.telegram.org/bot6166166487:AAGnLTIkj5dZfGmCK-Xo9FxIo4WZjal-Eck/sendMessage?chat_id=224039891&text=' + "Вам поступила заявка с сайта от клиента по имени " + name + ", телефон: " + phone;
+                    }
+                }
+        
+                if (!phone && mail) {
+                    if (mail.trim() != '') {
+                        link = 'https://api.telegram.org/bot6166166487:AAGnLTIkj5dZfGmCK-Xo9FxIo4WZjal-Eck/sendMessage?chat_id=224039891&text=' + "Вам поступила заявка с сайта от клиента по имени " + name + ", электронная почта: " + mail;
+                    }
+                }
+        
+                if (phone && mail) {
+                    if (phone.trim() != '' && mail.trim() != '') {
+                        link = 'https://api.telegram.org/bot6166166487:AAGnLTIkj5dZfGmCK-Xo9FxIo4WZjal-Eck/sendMessage?chat_id=224039891&text=' + "Вам поступила заявка с сайта от клиента по имени " + name + ", телефон: " + phone + ", электронная почта: " + mail;
+                    } 
+                }
+        
+            //конфигурация объекта
+            requestObj.open('GET', link, false);
+                
+            //отправка запроса
+            requestObj.send();
+
+            //скрыть форму
+            modalFeedbackForm.classList.add('d-none');
+
+            //открыть поп-ап об успешной отправки заявки
+            modalFeedbackFormSuccess.classList.remove('d-none');
+
+            //автоматически закрыть поп-ап через 3 сек
+            setTimeout(() => { 
+                modalFeedbackFormSuccess.classList.add('d-none'); 
+            }, 3000);
+
+            //если нужные поля не заполнены
+            } else {
+                document.getElementById('result').innerHTML = 'Для обратной связи необходимо указать почту или телефон.';
+            }
+
+    } else {
+        document.getElementById('result').innerHTML = 'Заполните поле "Имя"';
+    }
+
 }
